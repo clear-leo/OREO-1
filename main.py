d1931a58c3b4aa60637838e37c66a177846b19c2
clear-leo <g567zp@gmail.com>
2026-09-17 17:10:05 -0500

    Add code comments

--- main.py
+++ main.py
@@ -10,15 +10,18 @@ root.configure(background="#021D29")
 class Information(ttk.Frame):
     
     def __init__(self, master):
-        #CHANGEME
         self.WIDTH = 1850
         self.HEIGHT = 1080 
+
+        #CHANGEME
+        # Aqui esta toda la información que vamos a cambiar para añadir el Serial.
         self.pais = ttk.StringVar()
         self.info_pais = ttk.StringVar()
         self.imagen_pais = "stupid" + ".png"
         self.info_pais.set("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
         self.pais.set("Pais placeholder")
         # --
+
         self.master = master
 
         super().__init__(master, padding=16)
@@ -35,6 +38,9 @@ class Information(ttk.Frame):
         ttk.Label(frame, textvariable=self.pais, font=("Arial", 50, "bold"), anchor="center").grid(row=0, column=0, sticky='nsew', pady=20)
         ttk.Label(frame, textvariable=self.info_pais, font=("Arial", 24), anchor="w", wraplength=self.WIDTH).grid(row=1, column=0, sticky='nsew', pady=20)
 
+
+        # Es probable que esta lógica la cambie, y es más probable que esta lógica sea obsoleta cuando
+        # - añademos el cambio dinámico.
         imagen = Image.open(self.imagen_pais)
         imagen_tk = ImageTk.PhotoImage(imagen)
 
@@ -42,6 +48,9 @@ class Information(ttk.Frame):
         label_imagen.image = imagen_tk  # type: ignore[attr-defined]
         label_imagen.grid(row=3, column=0, sticky='nsew', pady=20)
 
+    # Función para probar la idea de una funcion recurrente que revise si hemos recibido un mensaje por serial.
+    # O la logica se hace aquí o se hace dentro de otra función. Probablemente otra función quizas hasta -
+    # - otro archivo, preferiblemente eso.
     def __CHANGEME(self):
         print("hi")
         self.master.after(100, self.__CHANGEME)
@@ -49,9 +58,6 @@ class Information(ttk.Frame):
     def run(self):
         self.master.after(100, self.__CHANGEME)
         self.master.mainloop()
-        
-
-
 
 
 Information(root).run()
