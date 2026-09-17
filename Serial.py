ad75c7f4a8a728a79690c74cf428248d899ccd9f
clear-leo <g567zp@gmail.com>
2026-09-17 18:20:18 -0500

    Added functionality to wait for ACM port.

--- Serial.py
+++ Serial.py
@@ -1,15 +1,26 @@
 import serial.tools.list_ports
+import time
 
 class Serial:
     def __init__(self, baud: int):
         self.port = self.__find_port()
         self.serial = serial.Serial(self.port, baud, timeout=0)
+        while self.serial.in_waiting < 1:
+            print("WAITING FOR DEVICE...")
+            time.sleep(0.1)
+        print(f"FOUND SIGNAL: {self.serial.readline()}")
+        signal = b"ITSGOTIME"
+        self.serial.write(signal)
+        print(f"SENT SIGNAL: {signal.decode()}")
 
     def __find_port(self):
         for port in serial.tools.list_ports.comports():
             if "ACM" in port.device:
                 return port.device
-        raise RuntimeError("No valid ACM port found.")
+        print("Serial not found, retrying...")
+        time.sleep(1)
+        port = self.__find_port()
+        return port
     
     def is_ready(self):
         if self.serial.in_waiting > 1:
